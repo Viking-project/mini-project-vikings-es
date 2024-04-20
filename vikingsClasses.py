2312095d14f3d68628ad7b258766e02d4063d273
mgparallel <mgparallel.pm7@gmail.com>
2024-04-20 14:49:35 +0200

    added soldier and viking

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -5,28 +5,37 @@ import random
 
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
-        # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage 
     
 
 # Viking
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
-
+        self.name = name
+        super().__init__(health, strength)
+        
     def battleCry(self):
-        # your code here
+        return "Odin Owns You All!"        
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage 
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+        
+    
+    
 
-# Saxon
+""" # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
@@ -79,5 +88,4 @@ class War2:
         # your code here
 
     pass
-
-
+ """
